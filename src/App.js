ef14a4e43c6117712518a82eb15ca74b6021ced0
AmandaCleto <amanda.clet@gmail.com>
2020-02-26 13:52:21 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends React.Component {
+  state = {
+    loading: true,
+    people: []
+  };
+
+  async componentDidMount() {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    this.setState({people: data, loading: false});
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <div className="center">loading...</div>;
+    }
+
+    if (!this.state.people.length) {
+      return <div>didn't get a person</div>;
+    }
+
+    return (
+      <div className="App">
+      <h1 className="center">Get API</h1>
+      <form className="form">
+          {this.state.people.map((person, i) => (
+            <div className="line" key={person.id}>
+              <h1 id="name">{person.name}</h1>
+              <label>Phone:<div type="number" id="phone" className="label" name="phone">{person.phone}</div><br></br></label>
+              <label>E-mail:<div type="text" id="email" className="label"  name="email">{person.email}</div><br></br></label>
+              <label>City:<div type="text" id="address" className="label"  name="address">{person.address.city}</div><br></br></label>
+              <label>Street:<div type="text" id="street" className="label"  name="street">{person.address.street}</div><br></br></label>
+              <label>Website:<div type="text" id="city" className="label"  name="city">{person.website}</div><br></br></label>
+            </div>
+          ))}
+      </form>
     </div>
-  );
+    );
+  }
 }
 
-export default App;
---
+++ src/readme.md
@@ -0,0 +1,3 @@
+#React_fetch-1
+
+The intuition of this project was to learn how to get API using fetch function.
